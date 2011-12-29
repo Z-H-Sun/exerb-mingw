8d993e75cc5a407199cbf50ab07983e5781a9d84
Luis Lavena <luislavena@gmail.com>
2011-12-29 14:43:02 -0300

    Detect loaded DLLs and include in exy manifest
    This feature was 'stolen' from Ocra:
    
    - http://ocra.rubyforge.org
    - https://github.com/larsch/ocra

--- lib/exerb/utility2.rb
+++ lib/exerb/utility2.rb
@@ -21,7 +21,7 @@ module Exerb::Utility2
       end
     end
 
-    return $LOADED_FEATURES.collect { |filename|
+    features = $LOADED_FEATURES.collect { |filename|
       case filename.downcase
       when /\.rb$/o  then type = "script"
       when /\.so$/o  then type = "extension-library"
@@ -43,6 +43,13 @@ module Exerb::Utility2
     }.reject { |type, filename, filepath|
       reject_list.index(File.expand_path(filepath))
     }
+
+    detected_libraries.each do |path|
+      dll = ["extension-library", File.basename(path).downcase, path]
+      features << dll
+    end
+
+    return features
   end
 
   def self.require_name(filename)
@@ -53,6 +60,53 @@ module Exerb::Utility2
     end
 
     filename
+  end
+
+  # Code copied and adapted from Ocra project
+  # http://ocra.rubyforge.org/
+  def self.loaded_libraries
+    require "Win32API"
+
+    enumprocessmodules = Win32API.new('psapi',    'EnumProcessModules', ['L','P','L','P'], 'B')
+    getmodulefilename  = Win32API.new('kernel32', 'GetModuleFileName',  ['L','P','L'], 'L')
+    getcurrentprocess  = Win32API.new('kernel32', 'GetCurrentProcess',  ['V'], 'L')
+
+    bytes_needed = 4 * 32
+    module_handle_buffer = nil
+    process_handle = getcurrentprocess.call
+
+    loop do
+      module_handle_buffer = "\x00" * bytes_needed
+      bytes_needed_buffer = [0].pack("I")
+
+      r = enumprocessmodules.call(process_handle, module_handle_buffer,
+            module_handle_buffer.size, bytes_needed_buffer)
+
+      bytes_needed = bytes_needed_buffer.unpack("I")[0]
+      break if bytes_needed <= module_handle_buffer.size
+    end
+
+    handles = module_handle_buffer.unpack("I*")
+    handles.select { |handle| handle > 0 }.map do |handle|
+      str = "\x00" * 256
+      modulefilename_length = getmodulefilename.call(handle, str, str.size)
+      File.expand_path str[0, modulefilename_length]
+    end
+  end
+
+  def self.detected_libraries
+    exec_prefix = RbConfig::CONFIG["exec_prefix"]
+    libruby_so  = RbConfig::CONFIG["LIBRUBY_SO"]
+    loaded      = loaded_libraries
+
+    detected = loaded.select { |path|
+      normalized = path.downcase
+      path.include?(exec_prefix) &&
+        File.extname(normalized) == ".dll" &&
+        File.basename(normalized) != libruby_so
+    }
+
+    return detected
   end
 end # Exerb::Utility2
 
