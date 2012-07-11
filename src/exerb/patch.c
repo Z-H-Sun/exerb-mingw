3b7de8709ee18e9eb85b3188c24ebd9ac5e6b3d7
Dusan D. Majkic <dmajkic@lovor.net>
2012-07-11 10:45:55 +0200

    Added wraping for rb_require

---
+++ src/exerb/patch.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+#include "ruby.h"
+#include "exerb.h"
+
+VALUE __real_rb_require(const char *fname);
+#ifdef RUBY19
+VALUE __real_rb_require_safe(VALUE, int);
+#endif
+
+VALUE
+__wrap_rb_require(const char *fname)
+{
+        VALUE fn = rb_str_new2(fname);
+        OBJ_FREEZE(fn);
+        return exerb_require_safe(fn, rb_safe_level());
+        // return __real_rb_require(fname);
+}
+
+#ifdef RUBY19
+
+VALUE
+__wrap_rb_require_safe(VALUE fname, int safe)
+{
+        //printf("my_require: %s\n", RSTRING_PTR(fname));
+        return exerb_require_safe(fname, safe);
+        //return __real_rb_require_safe(fname, safe);
+}
+
+#endif
